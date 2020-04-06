630f420123009eacb6c6c2a25be334ea09b3aea6
k-romero <imkevinromero@gmail.com>
2020-04-06 16:47:29 -0400

    stubbed out basic app

--- src/main/java/com/demo/employeeApp/repository/EmployeeRepository.java
+++ src/main/java/com/demo/employeeApp/repository/EmployeeRepository.java
@@ -1,4 +1,9 @@
 package com.demo.employeeApp.repository;
 
-public class EmployeeRepository {
+import com.demo.employeeApp.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee,Long> {
 }
